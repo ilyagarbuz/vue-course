617578393d757e731866e6221c5818b4a8cade19
Ilya <garbuzilya.work@gmail.com>
2022-06-12 20:41:57 +0300

    add home_module_3

---
+++ src/components/ProductsList.vue
@@ -0,0 +1,14 @@
+<template>
+    <ul class="catalog__list">
+      <ProductsItem v-for="(product, index) in products" :product="product" :key="index"/>
+    </ul>
+</template>
+
+<script>
+import ProductsItem from './ProductsItem.vue'
+
+export default {
+  props: ['products'],
+  components: { ProductsItem }
+}
+</script>
---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Радионяня Motorola MBP16',
+    price: 3690,
+    image: 'img/radio.jpg'
+  },
+  {
+    title: 'Ультразвуковая зубная щётка Playbrush Smart Sonic',
+    price: 5660,
+    image: 'img/toothbrush.jpg'
+  },
+  {
+    title: 'Смартфон Xiaomi Mi Mix 3 6/128GB',
+    price: 21790,
+    image: 'img/phone.jpg'
+  },
+  {
+    title: 'Электроскейт Razor Cruiser',
+    price: 24690,
+    image: 'img/board.jpg'
+  },
+  {
+    title: 'Смартфон Xiaomi Mi A3 4/64GB Android One',
+    price: 14960,
+    image: 'img/phone-2@2x.jpg'
+  },
+  {
+    title: 'Смартфон Xiaomi Redmi 6/128GB',
+    price: 16540,
+    image: 'img/phone-3.jpg'
+  },
+  {
+    title: 'Электрический дрифт-карт Razor Crazy Cart',
+    price: 39900,
+    image: 'img/bicycle.jpg'
+  },
+  {
+    title: 'Радионяня Motorola MBP16Гироскутер Razor Hovertrax 2.0',
+    price: 34090,
+    image: 'img/wheels.jpg'
+  },
+  {
+    title: 'Детский трюковой самокат Razor Grom',
+    price: 14990,
+    image: 'img/scooter.jpg'
+  },
+  {
+    title: 'Роллерсёрф Razor RipStik Air Pro',
+    price: 6690,
+    image: 'img/ripstik.jpg'
+  },
+  {
+    title: 'Наушники AirPods с беспроводным зарядным футляром',
+    price: 16650,
+    image: 'img/airpods.jpg'
+  },
+  {
+    title: 'Наушники Sony',
+    price: 16690,
+    image: 'img/headphones.jpg'
+  }
+]
